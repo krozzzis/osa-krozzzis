7c77888a2323c784634071d3f58d6d5a1a704c6d
krozzzis <schumov.nn@gmail.com>
2026-08-26 23:43:49 +0500

    feat(fonts): use global font options for gtk, qt, plymouth, wezterm
    Set user.fonts.regular to Inter and monospace to JetBrains Mono. Wire them into fontconfig, gtk.font, qt, plymouth.font and wezterm. Nerd Fonts handling preserved.

--- modules/dotfiles/wezterm.nix
+++ modules/dotfiles/wezterm.nix
@@ -4,12 +4,12 @@
 delib.module {
   name = "osa.terminal.wezterm";
 
-  home.ifEnabled = {
+  home.ifEnabled = { myconfig, ... }: {
     programs.wezterm.extraConfig = ''
       local wezterm = require 'wezterm'
       local config = wezterm.config_builder()
 
-      config.font = wezterm.font 'JetBrains Mono'
+      config.font = wezterm.font '${myconfig.user.fonts.monospace.name}'
       config.hide_tab_bar_if_only_one_tab = true
 
       config.colors = {
--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -6,6 +6,18 @@ delib.module {
     user.fonts.enable = delib.boolOption true;
   };
 
+  # Устанавливаем глобальные шрифты как просили: Inter + JetBrains Mono
+  myconfig.always = {
+    user.fonts.regular = {
+      pkg = pkgs.inter;
+      name = "Inter";
+    };
+    user.fonts.monospace = {
+      pkg = pkgs.jetbrains-mono;
+      name = "JetBrains Mono";
+    };
+  };
+
   nixos.ifEnabled = { myconfig, ... }: {
     fonts.packages = with pkgs; [
       noto-fonts
@@ -13,11 +25,9 @@ delib.module {
       noto-fonts-cjk-serif
       noto-fonts-color-emoji
       liberation_ttf
-
       twemoji-color-font
-
-      inter
-      jetbrains-mono
+      myconfig.user.fonts.regular.pkg
+      myconfig.user.fonts.monospace.pkg
     ] ++ lib.optionals myconfig.user.gui.fonts.nerdfonts [
       nerd-fonts.fira-code
       nerd-fonts.jetbrains-mono
@@ -27,10 +37,34 @@ delib.module {
     fonts.fontconfig = {
       defaultFonts = {
         serif     = [ "Noto Serif" "Noto Serif CJK SC" ];
-        sansSerif = [ "Inter" "Noto Sans CJK SC" ];
-        monospace = [ "JetBrainsMono Nerd Font" "Noto Sans Mono CJK SC" ];
+        sansSerif = [ myconfig.user.fonts.regular.name "Noto Sans CJK SC" ];
+        monospace = [
+          (if myconfig.user.gui.fonts.nerdfonts then "JetBrainsMono Nerd Font" else myconfig.user.fonts.monospace.name)
+          "Noto Sans Mono CJK SC"
+        ];
         emoji     = [ "Twemoji Mozilla" "Noto Color Emoji" ];
       };
     };
+  };
+
+  home.ifEnabled = { myconfig, ... }: {
+    # GTK / Qt — чтобы все приложения брали тот же шрифт, а не только fontconfig
+    gtk = {
+      enable = true;
+      font = {
+        name = myconfig.user.fonts.regular.name;
+        size = 11;
+      };
+    };
+
+    qt = {
+      enable = true;
+      platformTheme.name = "qtct";
+    };
+
+    home.packages = with pkgs; [
+      myconfig.user.fonts.regular.pkg
+      myconfig.user.fonts.monospace.pkg
+    ];
   };
 }
